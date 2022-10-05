1cdd868e80b869816a67a95e918b2ae599756c71
NoneNameDeveloper <lab@abitur-helper.ru>
2022-10-05 18:21:26 +0300

    Added anti-break in 1 lab homework

--- MyApp/home.py
+++ MyApp/home.py
@@ -1,14 +1,19 @@
-import math
- 
+import math, sys
+
 print("Введите коэффициенты для уравнения")
 print("ax^2 + bx + c = 0:")
 a = float(input("a = "))
 b = float(input("b = "))
 c = float(input("c = "))
- 
+
+if a == 0:
+	print("У вас не получится сломать мою программу! a не может равняться нулю.")
+	sys.exit(0)
+
+
 discr = b ** 2 - 4 * a * c
 print("Дискриминант D = %.2f" % discr)
- 
+
 if discr > 0:
     x1 = (-b + math.sqrt(discr)) / (2 * a)
     x2 = (-b - math.sqrt(discr)) / (2 * a)
